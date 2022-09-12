15f9c056b215ddb99305bc102d289502d084a9cb
Richard <info@monkemedia.co.uk>
2022-09-12 13:41:33 +0100

    added new page

---
+++ src/api/delivery-and-returns-page/controllers/delivery-and-returns-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  delivery-and-returns-page controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::delivery-and-returns-page.delivery-and-returns-page');
---
+++ src/api/delivery-and-returns-page/services/delivery-and-returns-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * delivery-and-returns-page service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::delivery-and-returns-page.delivery-and-returns-page');
