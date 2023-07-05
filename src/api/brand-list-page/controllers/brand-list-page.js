1288ea19f1f38a3c62fda9766c2ed3f0977fd67b
Richard <info@monkemedia.co.uk>
2023-07-05 07:56:51 +0100

    quick links added to category and brand pages

---
+++ src/api/brand-list-page/controllers/brand-list-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  brand-list-page controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::brand-list-page.brand-list-page');
---
+++ src/api/brand-list-page/services/brand-list-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * brand-list-page service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::brand-list-page.brand-list-page');
